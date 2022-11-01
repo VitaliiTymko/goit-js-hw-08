b43c6049f76096f84644bd83cc5bc5b114b4138b
VitaliiTymko <intehmet2015@gmail.com>
2022-11-01 22:30:47 +0200

    Finish DZ8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle';
 let Iframe = document.querySelector('iframe');
 const player = new Player(Iframe);
 
@@ -10,13 +10,17 @@ player.on('play', function () {
 //         console.log('title:', title);
 //     });
 
-const onPlay = function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-};
-player.on('timeupdate', onPlay);
+const onPlay = throttle((data)=>{
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000);
+
+player.on("timeupdate",  onPlay);
 
 player.on('loaded', function () {
   if (localStorage.getItem('videoplayer-current-time')) {
     player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
   }
 });
+
+
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+let formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+refs.form.addEventListener('submit', throttle(onFormSubmit, 500));
+
+savedFormData();
+
+refs.form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+});
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function savedFormData(){
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+    const parsedFormData = JSON.parse(savedFormData);
+    if(parsedFormData) {
+        formData = parsedFormData;
+        refs.email.value = formData.email || "";
+        refs.textarea.value = formData.message || "";
+        console.log(savedFormData);
+    }
+};
