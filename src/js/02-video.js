bf48ba568b207bc4d0962e6cc496563aa59cb762
VitaliiTymko <intehmet2015@gmail.com>
2022-11-01 21:20:14 +0200

    02 finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+let Iframe = document.querySelector('iframe');
+const player = new Player(Iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+// player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', onPlay);
+
+player.on('loaded', function () {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+});
